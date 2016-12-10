593762bba1ff46f93e53aed685f44fe0fc7eb028
Miguel Palau <mpalauzarza@gmail.com>
2016-12-09 22:50:30 -0600

    conditional on the cp to test

--- get.sh
+++ get.sh
@@ -4,11 +4,23 @@ NEOVIM=$DOTFILES/nvim
 
 echo 'Getting everything for you to commit'
 
-cp -v ~/{.gitignore_global,.tmux.conf,.zshrc,xterm-256color-italic.terminfo,colors.sh} $DOTFILES
+if cp -v ~/{.gitignore_global,.tmux.conf,.zshrc,xterm-256color-italic.terminfo,colors.sh} $DOTFILES; then
+  echo 'Copy code: $? - Successful'
+else
+  echo 'Something happened: $? - Unsuccessful'
+fi
 
-cp -Rv ~/.config/nvim/config $NEOVIM
-cp -v ~/.config/nvim/init.vim $NEOVIM
+if cp -Rv ~/.config/nvim/config $NEOVIM; then
+  echo 'Copy code: $? - Successful'
+else
+  echo 'Something happened: $? - Unsuccessful'
+fi
 
-echo "🎉🎉🍕😎"
-echo "All done! That wasn't hard was it? Now commit my friend!"
+if cp -v ~/.config/nvim/init.vim $NEOVIM; then
+  echo 'Copy code: $? - Successful'
+  echo "🎉🎉🍕😎"
+  echo "All done! That wasn't hard was it? Now commit my friend!"
+else
+  echo 'Something happened: $? - Unsuccessful'
+fi
 
