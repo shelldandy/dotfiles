9ed43197343b7b6570c6895fba8990d843a6e6e3
Miguel Palau <mpalauzarza@gmail.com>
2016-12-09 22:54:30 -0600

    verbose deployment

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 echo "I'm about to copy all the files to their respective places hang tight..."
 
-cp {.gitignore_global,.tmux.conf,.zshrc,xterm-256color-italic.terminfo,colors.sh} ~
+cp -v {.gitignore_global,.tmux.conf,.zshrc,xterm-256color-italic.terminfo,colors.sh} ~
 
-cp -R nvim/* ~/.config/nvim
+cp -Rv nvim/* ~/.config/nvim
 
 echo "All done! That wasn't hard was it?"
