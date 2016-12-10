e810de519f37de723c6728d6a00be85421780962
Miguel Palau <mpalauzarza@gmail.com>
2016-12-09 22:23:37 -0600

    adding a new script for deployment

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "I'm about to copy all the files to their respective places hang tight..."
+
+cp {.gitignore_global,.tmux.conf,.zshrc,xterm-256color-italic.terminfo,colors.sh} ~
+
+cp -R nvim/* ~/.config/nvim
+
+echo 'All done! That wasn\'t hard was it? 🍕'
