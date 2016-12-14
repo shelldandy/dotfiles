14a86295587a254f7ce981aebcdf3dbe17f6cc3f
Miguel Palau <mpalauzarza@gmail.com>
2016-12-13 21:24:03 -0600

    funnier deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo "I'm about to copy all the files to their respective places hang tight..."
 
 cp -v {.gitignore_global,.tmux.conf,.zshrc,xterm-256color-italic.terminfo,colors.sh} ~
-
+echo '⏲.... Almost there'
 cp -Rv nvim/* ~/.config/nvim
 
-echo "All done! That wasn't hard was it?"
+echo "🍕 🍺 All done! That wasn't that hard was it? 🎉"
