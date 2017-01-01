40257296a352a1997f9c372e16bbf68cbb706eb6
Miguel Palau <mpalauzarza@gmail.com>
2016-12-31 22:57:47 -0600

    added php debug ini file

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@
 echo "I'm about to copy all the files to their respective places hang tight..."
 
 cp -v {.gitignore_global,.tmux.conf,.zshrc,xterm-256color-italic.terminfo,colors.sh} ~
+
+echo "Let's take care of our PHP debug situation now..."
+cp -v ext-xdebug.ini /usr/local/etc/php/5.6/conf.d
+
 echo '⏲.... Almost there'
 cp -Rv nvim/* ~/.config/nvim
 
