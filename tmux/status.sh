14968f128df009660ededce26b0fdb8aab3d4a97
Miguel Palau <mpalauzarza@gmail.com>
2017-03-02 10:47:24 -0600

    tweaks on oceanic tmux theme

--- tmux/status.sh
+++ tmux/status.sh
@@ -4,12 +4,12 @@ source "$HOME/.tmux/segment.sh"
 
 displayStatus() {
   if [[ $1 == "left" ]]; then
-    displaySegment "session" "#282828" "#6699cc"
+    displaySegment "session" "#d8dee9" "#6699cc"
   else
-    displaySegment "time" "#ffffff" "#65737e"
-    displaySegment "cpu" "#ffffff" "#65737e"
-    displaySegment "battery" "#ffffff" "#65737e"
-    displaySegment "hostname" "#3c3836" "#6699cc"
+    displaySegment "time" "#d8dee9" "#65737e"
+    displaySegment "cpu" "#d8dee9" "#65737e"
+    displaySegment "battery" "#d8dee9" "#65737e"
+    displaySegment "hostname" "#d8dee9" "#6699cc"
   fi
 
   exit 0
