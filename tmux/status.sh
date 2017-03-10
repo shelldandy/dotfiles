f28a31f71b694aec4cca6e9b28648da8fe4a89c4
Miguel Palau <mpalauzarza@gmail.com>
2017-03-10 17:36:42 -0600

    tmux fixes on style 💅

--- tmux/status.sh
+++ tmux/status.sh
@@ -6,9 +6,9 @@ displayStatus() {
   if [[ $1 == "left" ]]; then
     displaySegment "session" "#d8dee9" "#6699cc"
   else
-    displaySegment "time" "#d8dee9" "#65737e"
     displaySegment "cpu" "#d8dee9" "#65737e"
     displaySegment "weather" "#d8dee9" "#65737e"
+    displaySegment "time" "#d8dee9" "#65737e"
     displaySegment "battery" "#d8dee9" "#65737e"
     displaySegment "hostname" "#d8dee9" "#6699cc"
   fi
