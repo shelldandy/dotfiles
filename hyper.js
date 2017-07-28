35c485ff2084e63aebcdfe81b11a5fae574074d3
Miguel Palau <mpalauzarza@gmail.com>
2017-07-28 12:11:17 -0500

    added borders

--- hyper.js
+++ hyper.js
@@ -12,47 +12,33 @@ module.exports = {
     css: '',
     termCSS: 'x-row {font-weight: 200}',
     padding: '12px 14px',
-    colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
-    },
+    colors: {},
     shell: '',
     shellArgs: ['--login'],
     env: {
-      HYPERTERM: 'vibrancy'
+      THEME: 'oceanic-next'
     },
     bell: 'SOUND',
     copyOnSelect: false,
     workingDirectory: '~/git',
     visor: {
       hotkey: 'CommandOrControl+Shift+Z'
+    },
+    hyperBorder: {
+      borderColors: ['#fc1da7', '#fba506'],
+      borderWidth: '5px'
     }
   },
 
   plugins: [
-    // Theme try 1
-    // 'hyperterm-oceanic-next',
-    // 'hyper-transparent-dynamic',
-    // Theme try 2
-    'hyper-sierra-vibrancy',
+    'hyperterm-oceanic-next',
     'hyper-font-smoothing',
     'hyperterm-working-directory',
     'hyperterm-visor',
-    'hyperterm-cursor'
+    'hyperterm-cursor',
+    'hyper-nobold',
+    'hyperterm-alternatescroll',
+    'hyperborder'
   ],
   localPlugins: []
 }
