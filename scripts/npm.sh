e17cdd18a8dd53741f2fb7a3d4660e8fb016fe98
Miguel Palau <mpalauzarza@gmail.com>
2017-09-18 08:02:56 -0500

    updating nodejs to 8.5.0

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
 
-nvm install 6.11.0
+nvm install 8.5.0
 
 
 # install global npm packages
 npm i -g npm
 npm i -g yarn
 npm i -g npm-check-updates
+npm i -g now
+npm i -g tern
