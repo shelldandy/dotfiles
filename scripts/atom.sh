318cbd0a64f52663811ceedbdd9ec49dff8bdae4
Miguel Palau <mpalauzarza@gmail.com>
2017-09-23 21:04:50 -0500

    newer packages for extra good life

--- scripts/atom.sh
+++ scripts/atom.sh
@@ -13,12 +13,15 @@ apm install gruvbox
 apm install highlight-selected
 apm install intentions
 apm install language-babel
+apm install language-viml
 apm install linter
 apm install linter-js-standard
 apm install linter-stylelint
 apm install linter-ui-default
 apm install no-title-bar
+apm install pigments
 apm install project-manager
 apm install relative-numbers
 apm install termination
 apm install vim-mode-plus
+apm install vim-mode-plus-keymaps-for-surround
