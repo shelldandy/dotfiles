1f80d11e9069c51aa2867be12abdd650c95f4719
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-08-21 01:28:36 -0500

    keep the shadows for pictures

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -2,5 +2,5 @@
 
 # Disable shadow on window screenshots
 # https://apple.stackexchange.com/questions/50860/how-do-i-take-a-screenshot-without-the-shadow-behind-it
-defaults write com.apple.screencapture disable-shadow -bool TRUE
-killall SystemUIServer
+# defaults write com.apple.screencapture disable-shadow -bool TRUE
+# killall SystemUIServer
--- scripts/npm.sh
+++ scripts/npm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Install nvm
-curl -o- 'https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh' | bash
-nvm install '8.9.4'
+curl -o- 'https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh' | bash
+nvm install '8.11.4'
