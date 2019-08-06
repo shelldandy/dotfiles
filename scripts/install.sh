d34d493265b33c32b6a4ba0c23ecd51afaa8abed
Miguel Palau Zarza <mpalauzarza@gmail.com>
2019-08-06 08:49:09 -0500

    more snippets

--- scripts/install.sh
+++ scripts/install.sh
@@ -47,9 +47,6 @@ source ./yarn.sh
 # install python packages
 source ./pip.sh
 
-# install wp-cli
-source ./wp-cli.sh
-
 # Neovim
 source ./nvim.sh
 
--- scripts/wp-cli.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
