8434cbd85aa7b1bab4e9f06777955677e60d9db2
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-06-04 09:16:54 -0600

    we can now remove "VeryLazy" from the simple plugins

--- config/nvim/lua/plugins/simple_plugins.lua
+++ config/nvim/lua/plugins/simple_plugins.lua
@@ -2,28 +2,22 @@ return {
   -- Show git blame in the current line
   {
     "f-person/git-blame.nvim",
-    event = "VeryLazy",
   },
   {
     "preservim/nerdcommenter",
-    event = "VeryLazy",
   },
   {
     "tpope/vim-surround",
-    event = "VeryLazy",
   },
   {
     "prisma/vim-prisma",
-    event = "VeryLazy",
   },
   -- Required for testing
   {
     "nvim-neotest/nvim-nio",
-    event = "VeryLazy",
   },
   {
     "ruifm/gitlinker.nvim",
-    event = "VeryLazy",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
       mappings = "<leader>gy",
