ea403425e7f57c48730a8a63baf9f133b725a352
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-06-12 12:05:04 -0600

    use lazyvim.pick

--- config/nvim/lua/plugins/package-info.lua
+++ config/nvim/lua/plugins/package-info.lua
@@ -3,15 +3,5 @@ return {
     "vuki656/package-info.nvim",
     ft = "json",
     dependencies = { "MunifTanjim/nui.nvim" },
-    config = function()
-      require("package-info").setup({
-        autostart = false,
-        package_manager = "yarn",
-        colors = {
-          outdated = "#db4b4b",
-        },
-        hide_up_to_date = true,
-      })
-    end,
   },
 }
