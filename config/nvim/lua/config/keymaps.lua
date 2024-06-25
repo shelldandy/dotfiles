5a0cea4a1ba4cd6946f8ba39df45042646687c92
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-06-25 08:53:46 -0600

    tweaking keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -8,33 +8,35 @@ keymap.set("n", "<left>", "<c-w>5<", { silent = true })
 keymap.set("n", "<up>", "<c-w>5+", { silent = true })
 keymap.set("n", "<down>", "<c-w>5-", { silent = true })
 
--- keymap.set(
---   "n",
---   "<leader>cpt",
---   require('package-info').toggle,
---   { silent = true, noremap = true, desc = "Toggle" }
--- )
--- keymap.set(
---   "n",
---   "<leader>cpd",
---   require('package-info').delete,
---   { silent = true, noremap = true, desc = "Delete package" }
--- )
--- keymap.set(
---   "n",
---   "<leader>cpu",
---   require('package-info').update,
---   { silent = true, noremap = true, desc = "Update package" }
--- )
--- keymap.set(
---   "n",
---   "<leader>cpi",
---   require('package-info').install,
---   { silent = true, noremap = true, desc = "Install package" }
--- )
--- keymap.set(
---   "n",
---   "<leader>cpv",
---   require('package-info').change_version,
---   { silent = true, noremap = true, desc = "Change version" }
--- )
+keymap.set(
+  "n",
+  "<leader>cpt",
+  function ()
+    require('package-info').toggle()
+  end,
+  { silent = true, noremap = true, desc = "Toggle" }
+)
+keymap.set(
+  "n",
+  "<leader>cpd",
+  require('package-info').delete,
+  { silent = true, noremap = true, desc = "Delete package" }
+)
+keymap.set(
+  "n",
+  "<leader>cpu",
+  require('package-info').update,
+  { silent = true, noremap = true, desc = "Update package" }
+)
+keymap.set(
+  "n",
+  "<leader>cpi",
+  require('package-info').install,
+  { silent = true, noremap = true, desc = "Install package" }
+)
+keymap.set(
+  "n",
+  "<leader>cpv",
+  require('package-info').change_version,
+  { silent = true, noremap = true, desc = "Change version" }
+)
