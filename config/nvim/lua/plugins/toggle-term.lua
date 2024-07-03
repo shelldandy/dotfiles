ea82a2874f20f3e22be53151d31ecd455dff3fee
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-07-03 15:27:12 -0600

    more useful term command

--- config/nvim/lua/plugins/toggle-term.lua
+++ config/nvim/lua/plugins/toggle-term.lua
@@ -37,7 +37,24 @@ return {
 
       -- Define keymaps
       vim.keymap.set("n", "<leader>;;", function()
-        vim.cmd("ToggleTermToggleAll")
+        vim.cmd([[ ToggleTermToggleAll ]])
+
+        -- list current buffers
+        local buffers = vim.api.nvim_list_bufs()
+
+        -- check if toggleterm buffer exists. If not then create one by vim.cmd [[ exe 1 . "ToggleTerm" ]]
+        local toggleterm_exists = false
+        for _, buf in ipairs(buffers) do
+          local buf_name = vim.api.nvim_buf_get_name(buf)
+          if buf_name:find("toggleterm#") then
+            toggleterm_exists = true
+            break
+          end
+        end
+
+        if not toggleterm_exists then
+          vim.cmd([[ exe 1 . "ToggleTerm" ]])
+        end
       end, { noremap = true, silent = true, desc = "Toggle all terminals" })
       vim.keymap.set(
         "n",
