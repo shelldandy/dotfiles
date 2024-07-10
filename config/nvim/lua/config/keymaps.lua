511bde7d6774f8bd46df7a51c1c97c2fcc8cf794
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-07-10 15:00:47 -0600

    use descriptions for the keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,9 @@
 local set = vim.keymap.set
 -- press jj to escape quickly
-set("i", "jj", "<esc>")
+set("i", "jj", "<esc>", { noremap = true, silent = true, desc = "Quickly escape" })
 
 -- adjust split sizes with arrow keys
-set("n", "<right>", "<c-w>5>", { silent = true })
-set("n", "<left>", "<c-w>5<", { silent = true })
-set("n", "<up>", "<c-w>5+", { silent = true })
-set("n", "<down>", "<c-w>5-", { silent = true })
+set("n", "<right>", "<c-w>5>", { silent = true, desc = "Increase window width" })
+set("n", "<left>", "<c-w>5<", { silent = true, desc = "Decrease window width" })
+set("n", "<up>", "<c-w>5+", { silent = true, desc = "Increase window height" })
+set("n", "<down>", "<c-w>5-", { silent = true, desc = "Decrease window height" })
