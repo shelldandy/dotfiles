fddaa522b8fdf5ed2e4779c8be23e0994de3f4b3
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-07-23 08:38:40 -0600

    placeholder change

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,1 +1,13 @@
 require("config.lazy")
+
+if vim.fn.filereadable(".vscode/launch.json") then
+  require("dap.ext.vscode").load_launchjs(nil, {})
+end
+
+-- Extends dap.configurations with entries read from .vscode/launch.json
+-- Remove when this PR is merged
+-- https://github.com/LazyVim/LazyVim/pull/4106
+local vscode = require("dap.ext.vscode")
+if vim.fn.filereadable(".vscode/launch.json") then
+  vscode.load_launchjs(nil, {})
+end
