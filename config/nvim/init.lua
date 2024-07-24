2f4a81952939e4585b7756052bd1dff1e9fde435
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-07-24 07:11:14 -0600

    remove vscode nvim-dap fix now that its on lazyvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,13 +1,1 @@
 require("config.lazy")
-
-if vim.fn.filereadable(".vscode/launch.json") then
-  require("dap.ext.vscode").load_launchjs(nil, {})
-end
-
--- Extends dap.configurations with entries read from .vscode/launch.json
--- Remove when this PR is merged
--- https://github.com/LazyVim/LazyVim/pull/4106
-local vscode = require("dap.ext.vscode")
-if vim.fn.filereadable(".vscode/launch.json") then
-  vscode.load_launchjs(nil, {})
-end
