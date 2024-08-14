9d23b73630054f9bd4f2b2f44a5d64263abaad27
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-08-14 16:08:39 -0600

    remove unused stuff

--- themes/gruvbox-light.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Vim
-export THEME_COLORS="gruvbox-light"
-
-export VIM_ALE_ERROR_FG="#fb4934"
-export VIM_ALE_ERROR_BG="#3c3836"
-export VIM_ALE_WARNING_FG="#fe8019"
-export VIM_ALE_WARNING_BG="#3c3836"
--- themes/oceanic-next.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Vim
-export THEME_COLORS="oceanic-next"
-
-export VIM_ALE_ERROR_FG="#ec5f67"
-export VIM_ALE_ERROR_BG="NONE"
-export VIM_ALE_WARNING_FG="#f7c862"
-export VIM_ALE_WARNING_BG="NONE"
