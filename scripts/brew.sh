c840c3f9221cb68861d51eded57c8802fb5df10a
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-08-14 16:00:31 -0600

    tweak brews

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -12,13 +12,9 @@
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Taps
-#brew tap 'neovim/neovim'
-#brew tap 'thoughtbot/formulae'
 brew tap 'homebrew/cask'
 brew tap 'homebrew/cask-versions'
 brew tap 'homebrew/cask-fonts'
-#brew tap 'eddieantonio/eddieantonio'
-#brew tap 'koekeishiya/formulae'
 
 # Installs
 ## System Tools
@@ -26,17 +22,8 @@ brew install 'font-fira-code-nerd-font' --cask
 
 # Casks
 brew install 'balenaetcher' --cask # Burn ISO images to USB Drives
-brew install 'contexts' --cask     # Better cmd-tab
-brew install 'dash' --cask         # Docs
 brew install 'docker' --cask
-brew install 'dropbox' --cask
 brew install 'firefox' --cask
-brew install 'figma' --cask
-brew install 'focus' --cask
-brew install 'google-chrome' --cask
-brew install 'iterm2-nightly' --cask
-brew install 'rocket' --cask
 brew install 'slack' --cask
-brew install 'subtitles' --cask
 brew install 'telegram' --cask
 brew install 'transmission' --cask
