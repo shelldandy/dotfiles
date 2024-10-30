4c67dc33ee41203daee459870d2776dff14f7b56
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-10-30 13:17:27 -0600

    add docker and prisma to lazy langs

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.prisma" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     --{ import = "lazyvim.plugins.extras.linting.eslint" },
--- config/nvim/lua/plugins/simple_plugins.lua
+++ config/nvim/lua/plugins/simple_plugins.lua
@@ -1,8 +1,6 @@
 return {
   -- Show git blame in the current line
   { "f-person/git-blame.nvim" },
-  -- Prisma DB syntax highlighting and LSP
-  { "prisma/vim-prisma" },
   -- Required for testing
   { "nvim-neotest/nvim-nio" },
   { "zenbones-theme/zenbones.nvim" },
