fd720369968ee703ace58e7432e9f2a9d97700d0
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-10-30 13:15:20 -0600

    add docker lang

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     --{ import = "lazyvim.plugins.extras.linting.eslint" },
