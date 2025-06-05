5ee9d50f73131dd241c56c3c19893e111a681b6c
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-06-06 01:35:02 +0200

    working mode cc

--- config/nvim/lua/plugins/code-companion.lua
+++ config/nvim/lua/plugins/code-companion.lua
@@ -1,10 +1,27 @@
 return {
   {
     "olimorris/codecompanion.nvim",
-    opts = {},
+    opts = {
+      strategies = {
+        -- Change the default chat adapter
+        chat = {
+          adapter = function()
+            return require("codecompanion.adapters").extend("anthropic", {
+              env = {
+                api_key = "cmd:gpg --batch --quiet --decrypt ~/.claude.gpg",
+              },
+            })
+          end,
+        },
+      },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "codecompanion" },
   },
 }
