dee5c8bcf7573205fd2bcbef130da9394676f619
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-06-06 02:34:53 +0200

    add img processing

--- config/nvim/lua/plugins/code-companion.lua
+++ config/nvim/lua/plugins/code-companion.lua
@@ -69,4 +69,16 @@ return {
       file_types = { "markdown", "codecompanion" },
     },
   },
+  {
+    "HakonHarnes/img-clip.nvim",
+    opts = {
+      filetypes = {
+        codecompanion = {
+          prompt_for_file_name = false,
+          template = "[Image]($FILE_PATH)",
+          use_absolute_path = true,
+        },
+      },
+    },
+  },
 }
