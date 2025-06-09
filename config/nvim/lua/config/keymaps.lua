6b1b99a23e05d02ae86110d2ecd4129c65c5a962
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-06-09 12:04:34 -0600

    add claude quick toggle thing

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,14 @@ set("n", "<right>", "<c-w>5>", { silent = true, desc = "Increase window width" }
 set("n", "<left>", "<c-w>5<", { silent = true, desc = "Decrease window width" })
 set("n", "<up>", "<c-w>5+", { silent = true, desc = "Increase window height" })
 set("n", "<down>", "<c-w>5-", { silent = true, desc = "Decrease window height" })
+
+-- toggle floating terminal with claude
+set("n", "<C-;>", function()
+  Snacks.terminal("claude", { cwd = vim.fn.getcwd() })
+end, { desc = "Toggle Claude terminal" })
+
+-- toggle out of terminal mode with ctrl+q in terminal
+set("t", "<C-;>", function()
+  vim.cmd("stopinsert")
+  vim.cmd("close")
+end, { desc = "Close terminal and return to previous buffer" })
