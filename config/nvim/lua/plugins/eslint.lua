bd5ca580320291f8208789f4258a94527ea7a058
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-06-09 06:02:26 -0600

    add eslint

---
+++ config/nvim/lua/plugins/eslint.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = { eslint = {} },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+}
