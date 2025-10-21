dc92b680972ea3a640866ba9d6b63533492846a2
Miguel Palau Zarza <mpalauzarza@gmail.com>
2025-10-21 10:08:57 -0600

    claude keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -8,21 +8,6 @@ set("n", "<left>", "<c-w>5<", { silent = true, desc = "Decrease window width" })
 set("n", "<up>", "<c-w>5+", { silent = true, desc = "Increase window height" })
 set("n", "<down>", "<c-w>5-", { silent = true, desc = "Decrease window height" })
 
--- toggle floating terminal with claude
-set("n", "<C-;>", function()
-  Snacks.terminal.toggle("claude", { cwd = vim.fn.getcwd() })
-end, { desc = "Toggle Claude terminal" })
--- toggle floating terminal with claude
-set("n", "<C-'>", function()
-  Snacks.terminal.toggle("claude -c", { cwd = vim.fn.getcwd() })
-end, { desc = "Toggle Claude terminal" })
-
--- toggle out of terminal mode with ctrl+q in terminal
-set("t", "<C-;>", function()
-  vim.cmd("stopinsert")
-  vim.cmd("close")
-end, { desc = "Close terminal and return to previous buffer" })
-
 -- copy current buffer path to clipboard
 set("n", "<leader>cb", function()
   local buf_path = vim.api.nvim_buf_get_name(0)
@@ -34,3 +19,7 @@ set("n", "<leader>cb", function()
   vim.fn.setreg("+", relative_path)
   vim.notify("Copied to clipboard: " .. relative_path)
 end, { desc = "Copy current buffer path to clipboard" })
+
+-- ClaudeCode
+vim.keymap.set("n", "<leader>ac", "<cmd>ClaudeCode<CR>", { desc = "Toggle Claude Code" })
+vim.keymap.set("n", "<leader>aC", "<cmd>ClaudeCodeContinue<CR>", { desc = "Claude Code Continue" })
